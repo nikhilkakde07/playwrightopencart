e46f220cfd3e1a10bc9e2475f295581929e4dda1
Nikhil <nikhilkakde967@gmail.com>
2026-06-10 01:12:55 +0530

    code changes 6

---
+++ tests/gitHubsequence.spec.ts
@@ -0,0 +1,16 @@
+
+
+/* //name.yml -- Info about the pipeline
+steps
+stages
+env */
+
+//GIt actions 
+
+//1.Git init
+//2.git remote add origin URL with no inverted commas
+//FIrst 2 steps are for new repo for existing use below commands
+//3.git status - to check status
+//4.git add.
+//5.git commit -m "Reason of commit"
+//6.git push origin master
